16261ab728e37bc1345ce2f9e00b7c22d05a8dd5
Maxime Motte <maxeen17@gmail.com>
2018-10-02 22:24:10 +0200

    Suppr. anciens commentaires

--- js/main.js
+++ js/main.js
@@ -55,39 +55,3 @@ fetch('https://api.citybik.es/v2/networks/bicloo').then(function(res) {
 
 
 
-
-
-
-
-// var currentLi = document.createElement('div');
-//             currentLi.classList.add("station");
-//             var stationName = stations[i].extra.address;
-//             var emptySlot = stations[i].empty_slots;
-//             var freeBike =  stations[i].free_bikes;
-//             // var texte = (stations[i].extra.address + ' // ' + stations[i].empty_slots + ' // ' + stations[i].free_bikes);
-//             currentLi.textContent ='Nom de la station : ' + stationName + 'Emplacements disponibles : ' + emptySlot + 'Vélos disponibles : ' + freeBike;
-//             liste.appendChild(currentLi);
-
-
-// var currentLi = document.createElement('div');
-//             currentLi.classList.add("station");
-//             var texte = (stations[i].extra.address + ' – ' + stations[i].empty_slots + ' – ' + stations[i].free_bikes);
-//             currentLi.textContent = texte;
-//             liste.appendChild(currentLi);
-
-
-
-var Dailyweather = function(data) {
-    var todayDiv = document.querySelector('#today');
-
-    var stationName = document.createElement('h4');
-    stationName.textContent = 'Nom station : ' + stations[i].extra.address;
-    todayDiv.appendChild(stationName);
-
-    var freeBike = document.createElement('p');
-    freeBike.textContent = 'Vélos disponibles : ' + stations[i].free_bikes;
-    todayDiv.appendChild(freeBike);
-
-    var placeDispo = document.createElement('p');
-    placeDispo.textContent = 'Places disponibles : ' + stations[i].empty_slots;
-  };
